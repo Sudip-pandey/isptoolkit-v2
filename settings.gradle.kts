dcb25f033eb6bfd7d0848be4e6f3dffbe06adb99
Pandey Sudip <90173813+Sudip-pandey@users.noreply.github.com>
2026-09-03 20:42:51 +0545

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.5.2"
+        id("org.jetbrains.kotlin.android") version "2.0.0"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+        id("com.google.dagger.hilt.android") version "2.51.1"
+        id("com.google.devtools.ksp") version "2.0.0-1.0.22"
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
